b4c007e5de367ad37347448b02f937573737b885
Evelyn Bauer <evybauer@gmail.com>
2019-12-11 21:02:38 +0000

    WIP because branch mess

--- express_server.js
+++ express_server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
+
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
 
 function generateRandomString() {
   return Math.random().toString(36).substring('0', '8').replace('0.', '')
@@ -27,17 +31,22 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = {
+    username: req.cookies["username"],
     urls: urlDatabase
   }
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => { 
-  res.render("urls_new");
+  let templateVars = { 
+    username: req.cookies["username"],    
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { 
+    username: req.cookies["username"],
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL]     
   };
@@ -55,7 +64,7 @@ app.get("/hello", (req, res) => {
 
 
 app.post("/urls", (req, res) => {
-  let randStr = generateRandomString();
+  const randStr = generateRandomString();
   urlDatabase[randStr] = req.body.longURL
   res.redirect("/urls");         
 });
@@ -66,11 +75,27 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
+    const shortURL = req.params.shortURL;
     const longURL = req.body.edited
     urlDatabase[shortURL] = longURL;
   res.redirect("/urls");         
 });
+
+app.post("/login", (req, res) => {
+  const username = req.body.username
+  if (username) {
+    res.cookie('username', username);
+    res.redirect("/urls");
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+res.redirect("/urls");         
+});
+
 
 
 app.listen(PORT, () => {
