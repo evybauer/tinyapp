a31388fabe590016cf9afc489b3c6cfcb7640a05
Evelyn Bauer <evybauer@gmail.com>
2019-12-22 21:25:24 +0000

    Add functions generateRandomString and urlsForUser(id, urlDatabase)

--- helper.js
+++ helper.js
@@ -7,5 +7,23 @@ const getUserByEmail = function(email, database) {
   return null;
 };
 
+function generateRandomString() {
+  return Math.random().toString(36).substring('0', '8').replace('0.', '');
+}
+// Generates random string for user_id and shortURL
 
-module.exports = { getUserByEmail };
+
+function urlsForUser(id, urlDatabase) {
+  const filteredDatabase = {};
+
+  for (let shortURL in urlDatabase) {
+    const url = urlDatabase[shortURL];
+    if (url.userID === id)
+      filteredDatabase[shortURL] = url;
+  }
+  return filteredDatabase;
+}
+// Filter URL Database and check all short and long URLs per user id
+
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser };
