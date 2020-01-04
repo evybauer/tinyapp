c01c9c9dbddfda2cd45d1ac3a8680b87e590e144
Evelyn Bauer <evybauer@gmail.com>
2020-01-04 06:15:16 +0000

    Add databases into separate file, export modules

---
+++ sample.js
@@ -0,0 +1,28 @@
+// ----------  DATABASES ---------- //
+
+const users = {
+  'elB92y': {
+    id: 'elB92y',
+    email: 'user@example.com',
+    password: 'user-password'
+  },
+  '9Sa3Bf': {
+    id: '0Se7Gs',
+    email: 'user2@example.com',
+    password: 'user2password'
+  }
+};
+
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+  }
+};
+
+exports.users = users;
+exports.urlDatabase = urlDatabase;
